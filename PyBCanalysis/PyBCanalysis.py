b658f05a66ce370b0464a7d01a4364b1a41a0452
jie <jiejiezhangsta@gmail.com>
2025-07-04 18:06:42 +0800

    add cryo setup with python

--- PyBCanalysis/PyBCanalysis.py
+++ PyBCanalysis/PyBCanalysis.py
@@ -1,10 +1,25 @@
+# ---------------------------------------
 # print("Hello, this is PyBCanalysis!")
 # import sys
 # from math import cos, radians
 
 # for i in range(360):
 #     print(cos(radians(i)))
+# ---------------------------------------
 
+# # install cypo as a py package
+# py -m pip install maturin
+# py -m pip install cryo
+import maturin
+import cryo # use as cryo <dataset> [OPTIONS]
+# cryo currently obtains all of its data using the JSON-RPC protocol standard.
+# 1. set up cryo
+# 2. then use the cryo_python library to interact with it. 
+# *. cryo is a tool written in Rust that extracts data from blockchains via JSON-RPC requests, 
+#     processes it, and saves it in formats like Parquet, CSV, or JSON, or directly to Python 
+#     data structures. 
+
+# To use cryo you’ll need a custom RPC (Remote Procedure Call) endpoint.
 set ETH_RPC_URL=https://ethereum-mainnet.core.chainstack.com/36d94383e5200effe4a966a88f74e598
 
 cryo blocks --blocks 18734075:18735075 --dry
